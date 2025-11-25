7c0fc5a9858827e30b3eeca9497b1344b9b6fefc
re1naaa <Reinabeadini@gmail.com>
2025-11-25 17:34:08 +0100

    m

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <footer>
     <p>Digital School</p>
+    <nav class="footer-menu">
+    <?php wp_nav_menu(array('theme_location'=> 'wp_devs_footer_menu','deepth'=> 1));?>
+</nav>
 </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,5 +4,6 @@
         <meta charset="UTF-8">
         <?php wp_head(); ?>
         <title>Document</title>
+        <?php wp_nav_menu(array('theme_location'=> 'wp_devs_main_menu','deepth'=> 2));?>
     </head>
     <body>

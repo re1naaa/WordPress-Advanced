d25cd1c5dc4c1288ad28680fad1764015dd132d3
re1naaa <Reinabeadini@gmail.com>
2025-12-09 17:31:29 +0100

    FIKRETE

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<div id="content" class="site-content">
+    <div id="primary" clas="content-area">
+        <main id="main" class="side-main">
+            <div class="container"> 
+                <div class="error-404">
+                    <header>
+                        <h1>Page not found</h1>
+                        <p>Unfortutantely, the page you tried to reach does not exsist on this site.</p>
+                    </header>
+
+                    <div class="error">
+                        <p>How about doing a search?</p>
+                        <?php get_search_form(); ?>
+                        <?php 
+                        the_widget(
+                            'WP_Widget_Recent_Posts',
+                            array(
+                                'title' => 'Latest Posts',
+                                'number' => 3
+                            )
+                            );
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
